b9577571eb951d6bcf3e89e71424099682eb787e
dawson270500 <51200862+dawson270500@users.noreply.github.com>
2020-07-26 22:58:22 +0100

    Keybinds added, and save as

---
+++ Main.java
@@ -0,0 +1,63 @@
+package bText8;//Just class starter, doesnt do anything else
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class Main {
+	private static Gui win;
+	
+	public static void main(String args[]) {
+		win = new Gui();
+		  win.ta.addKeyListener(new KeyListener() {
+		
+		        @SuppressWarnings("deprecation")
+				@Override
+		        public void keyPressed(KeyEvent e) {
+		            if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {//Save keybind
+		            	if(win.curF != null) {//Save file open
+			            	if(!win.curF.save(win.ta.getText())) {
+			                	JOptionPane.showMessageDialog(null, "Saving failed, check file permissions");
+			                }else {
+			                	JOptionPane.showMessageDialog(null, "Saved successfully");
+			                }
+		            	}else {//Save as
+		            		int returnVal = win.fc.showOpenDialog(win.frame);
+		        	        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		        	            File file = (File) win.fc.getSelectedFile(); 
+		        	            FileHand fh = new FileHand(file);
+		        	            if(!fh.save(win.ta.getText())) {
+		        	            	JOptionPane.showMessageDialog(null, "Saving failed, check file permissions");
+		        	            }else {
+		        	            	JOptionPane.showMessageDialog(null, "Saved successfully");
+		        	            }
+		        	        }
+		            	}
+		        }else if ((e.getKeyCode() == KeyEvent.VK_O) && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {//Open keybind
+	        		int returnVal = win.fc.showOpenDialog(win.frame);
+	    	        if (returnVal == JFileChooser.APPROVE_OPTION) {
+	    	            File file = (File) win.fc.getSelectedFile();
+	    	            win.curF = new FileHand(file);
+	    	            win.ta.setText(win.curF.open());
+		        	}
+		        }
+		    }
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+					
+			}
+
+		});
+	}
+}
