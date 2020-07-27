231f4c14c29786894d3de53dbbfd2fdc3916ebba
dawson270500 <51200862+dawson270500@users.noreply.github.com>
2020-07-27 16:30:48 +0100

    Forgot to upload file to right folder

--- src/bText8/Main.java
+++
@@ -1,9 +0,0 @@
-package bText8;//Just class starter, doesnt do anything else
-
-public class Main {
-	private static Gui win;
-	
-	public static void main(String args[]) {
-		win = new Gui();
-	}
-}
